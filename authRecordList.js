c49deef15221599dfa8484412e9c6ca03280e094
Dirk E <dd1342.2@gmail.com>
2018-03-29 16:57:55 +0200

    v0.1.8 Added authrecordlist

--- authRecordList.js
+++ authRecordList.js
@@ -1,21 +1,21 @@
 module.exports = function (RED) {
-    function clientList(config) {
+    function authRecordList(config) {
         RED.nodes.createNode(this, config);
         var nde = this;
         //this.error("Entering wlanclientlist");
         var server = RED.nodes.getNode(config.server);
-        this.status({fill:"yellow",shape:"dot",text:"No WLAN client list retrieved"});
+        this.status({fill:"yellow",shape:"dot",text:"No auth record list retrieved"});
 
         nde.on('input', function (msg) {
-            this.status({fill:"yellow",shape:"dot",text:"Retrieving WLAN client list from OV ..."});
-            server.ov.getWlanClientList(server.ov.mode).then((result)=> {
-                console.log("Retrieved %d WLAN client list from OV", result.length);
-                this.status({fill:"green",shape:"dot",text:"Retrieved WLAN Client list from OV:" + result.length});
+            this.status({fill:"yellow",shape:"dot",text:"Retrieving auth record list from OV ..."});
+            server.ov.getAuthRecordList().then((result)=> {
+                console.log("Retrieved %d auth record from OV", result.length);
+                this.status({fill:"green",shape:"dot",text:"Retrieved auth records from OV:" + result.length});
                 msg.payload = result;
                 this.send(msg);
             }).catch(err=>{
-                console.log("Error retrieving WLAN Client list from OV", err);
-                this.status({fill:"red",shape:"dot",text:"Error retrieving WLAN Client list"});
+                console.log("Error retrieving auth record list from OV", err);
+                this.status({fill:"red",shape:"dot",text:"Error retrieving auth record list"});
             });
 
         });
@@ -29,5 +29,5 @@ module.exports = function (RED) {
         });
     }
 
-    RED.nodes.registerType("ov2500-wlanclientlist", clientList);
+    RED.nodes.registerType("ov2500-authrecordlist", authRecordList);
 };
