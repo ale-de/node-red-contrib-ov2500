bc66294d13fcda6aced0e302eb48009276c4de47
Dirk E <dd1342.2@gmail.com>
2018-03-30 10:50:25 +0200

    Release 0.1.9

---
+++ addAccount.js
@@ -0,0 +1,33 @@
+module.exports = function (RED) {
+    function addAccount(config) {
+        RED.nodes.createNode(this, config);
+        var nde = this;
+        let params = msg.ovparams;
+        var server = RED.nodes.getNode(config.server);
+        this.status({fill:"yellow",shape:"dot",text:"No account added to OV"});
+
+        nde.on('input', function (msg) {
+            this.status({fill:"yellow",shape:"dot",text:"Adding account to OV ..."});
+            server.ov.addAccount(params.usr,params.pwd,params.profile,params.policy).then((result)=> {
+                console.log("Added auth record to OV");
+                this.status({fill:"green",shape:"dot",text:"Added account to OV"});
+                msg.payload = result;
+                this.send(msg);
+            }).catch(err=>{
+                console.log("Error adding account to OV", err);
+                this.status({fill:"red",shape:"dot",text:"Error adding account to OV"});
+            });
+
+        });
+        nde.on('close', function (removed, done) {
+            if (removed) {
+                // This node has been deleted
+            } else {
+                // This node is being restarted
+            }
+            done();
+        });
+    }
+
+    RED.nodes.registerType("ov2500-addaccount", addAccount);
+};
